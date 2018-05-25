a022d2969620b811a6493ed2c6d20dacfb9970a4
vaishali0810 <vaishali.k.iyer@gmail.com>
2018-05-25 12:22:53 +0200

    Initiale commit neu

---
+++ app/src/main/java/com/example/mm/notenrechnerneu/Notensettings.java
@@ -0,0 +1,13 @@
+package com.example.mm.notenrechnerneu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Notensettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notensettings);
+    }
+}
